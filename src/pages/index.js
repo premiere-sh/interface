27bf994a83c5bcb19b347a3f2d99a0f42f1278ee
Piotr <piotr.ostrowski@aol.com>
2021-09-21 15:54:32 +0200

    add margin

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,7 +23,9 @@ export default function Home({ games, tournaments, player }) {
       <Container>
         <BigPlayerOfTheWeek player={player} />
       </Container>
-      <SocialsSection />
+      <div style={{ marginBottom: 150 }}>
+        <SocialsSection />
+      </div>
       <Footer />
     </Column>
   )
