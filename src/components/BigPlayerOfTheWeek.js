cd801a7d30f5e716025683958f89dd51be666863
Piotr <piotr.ostrowski@aol.com>
2021-09-21 15:25:53 +0200

    add player of the week

---
+++ src/components/BigPlayerOfTheWeek.js
@@ -0,0 +1,110 @@
+import Image from 'next/image'
+import styled from 'styled-components'
+import { LeaderboardTitle, Row, Column, GradientText } from 'components/common'
+
+const Container = styled.div`
+  margin-bottom: 150px;
+`
+
+const PlayerRow = styled(Row)``
+
+const StatsRow = styled(Row)`
+  margin-top: 30px; 
+`
+
+const PlayerName = styled.div`
+  font-family: Inter;
+  font-style: italic;
+  font-weight: bold;
+  font-size: 36px;
+  line-height: 100%;
+  color: ${props => props.theme.colors.black};
+  margin-bottom: 8px;
+`
+
+const MemberSince = styled.div`
+  font-family: Inter;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 150%;
+  color: ${props => props.theme.colors.gray};
+`
+
+const StatColumn = styled(Column)`
+  margin-right: 29px;
+`
+
+const StatHeading = styled.div`
+  font-family: Inter;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 150%;
+  letter-spacing: 0.1em;
+  color: ${props => props.theme.colors.gray};
+  text-transform: uppercase;
+`
+
+const Stat = styled.div`
+  font-family: Inter;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 150%;
+  color: ${props => props.theme.colors.black};
+`
+
+export default function BigPlayerOfTheWeek({ player }) {
+  return (
+    <Container>
+      <LeaderboardTitle>
+        Player of the week
+      </LeaderboardTitle>
+      <PlayerRow>
+        <div style={{ marginRight: 40 }}>
+          <Image 
+            src={'/devonhenry_.svg'}
+            width={220}
+            height={220}
+            alt={'player-of-the-week'}
+          />
+        </div>
+        <Column>
+          <PlayerName>
+            {player.name}
+          </PlayerName>
+          <MemberSince>
+            {player.since}
+          </MemberSince>
+          <StatsRow>
+            <StatColumn>
+              <StatHeading>
+                rank
+              </StatHeading>
+              <Stat>
+                {player.rank}
+              </Stat>
+            </StatColumn>
+            <StatColumn>
+              <StatHeading>
+                weekly wins
+              </StatHeading>
+              <Stat>
+                {player.weeklyWins}
+              </Stat>
+            </StatColumn>
+            <StatColumn>
+              <StatHeading>
+                $prem earned
+              </StatHeading>
+              <Stat>
+                {player.premEarned}
+              </Stat>
+            </StatColumn>
+          </StatsRow>
+        </Column>
+      </PlayerRow>
+    </Container>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,12 @@ import HomeHeading from 'components/HomeHeading'
 import WelcomeToPremiere from 'components/WelcomeToPremiere'
 import FeaturedGames from 'components/FeaturedGames'
 import FeaturedTournaments from 'components/FeaturedTournaments'
+import SocialsSection from 'components/SocialsSection'
+import BigPlayerOfTheWeek from 'components/BigPlayerOfTheWeek'
 import { getTournaments } from 'pages/tournaments'
 import { getGames } from 'pages/games'
 
-export default function Home({ games, tornaments }) {
+export default function Home({ games, tournaments, player }) {
   return (
     <Column> 
       <Header home={true} />
@@ -18,6 +20,10 @@ export default function Home({ games, tornaments }) {
       </Container>
       <FeaturedGames games={games} />
       <FeaturedTournaments tournaments={tournaments} />
+      <Container>
+        <BigPlayerOfTheWeek player={player} />
+      </Container>
+      <SocialsSection />
       <Footer />
     </Column>
   )
@@ -26,8 +32,15 @@ export default function Home({ games, tornaments }) {
 export async function getStaticProps(context) {
   const games = getGames()
   const tournaments = getTournaments()
+  const player = {
+    name: 'devonhenry_',
+    since: 'UK Member since August 24, 2021',
+    rank: '1st',
+    weeklyWins: '98',
+    premEarned: '2310994'
+  }
   return {
-    props: { games, tournaments }
+    props: { games, tournaments, player }
   }
 }
 
