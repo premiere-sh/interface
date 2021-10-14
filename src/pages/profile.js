140bc6344d5f4b159ced7a8f4ab6704f568c98bd
Jakub Frackowiak <xfragless@gmail.com>
2021-10-15 00:31:32 +0200

    margin for SocialsSection

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,10 +1,17 @@
-import Header from 'components'
+import Header from 'components/Header'
 import Teams from 'components/Teams'
+import Footer from 'components/Footer'
+import SocialsSection from 'components/SocialsSection'
 
 export default function profile() {
   return (
     <div>
-      <Teams/>      
+      <Header/>
+      <Teams/>
+      <div style={{marginBottom: 152, marginTop: 152}}>
+        <SocialsSection/>
+      </div>
+      <Footer/>
     </div>
   )
 }
