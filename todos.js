036a41e36416fad27ea49d33a5c76fa294ed55c1
Piotr <piotr.ostrowski@aol.com>
2021-10-25 14:19:29 +0200

    add todos

---
+++ todos.js
@@ -0,0 +1,42 @@
+// TODO
+// make the borders round for small tournament
+//
+// games 
+// keep them fixed, warzone to start with, remove filter by platform
+//
+// tournaments
+// POST view for creating tournaments
+// after post view, it also inits tournament contract
+// each tournament object stores tournamentContractAddress
+// 
+// tournament contract: 
+//   entryPrice
+//   allows to join 100 players
+//   currentPlayerId
+//   stores the date and time
+//   finalise(address winner) => withdraw(winner)
+//
+// GET view for retrieving tournaments
+//
+// leaderboard 
+// use Warzone call of duty api
+//
+// player of the week - most tournmanets won
+//
+// users
+// user_id
+// point system, points for winning tournaments
+// if we have more users this is st. change
+// rank system -> based on retrieved players, 
+// the one with most points is gonna be 1st
+//
+// prem earned delete for now
+// 
+// profile 
+// friends => mechanism for adding friends
+//
+// for now solos
+// 
+// recommended events: 
+// events.map((event) =>  Date.now() - event.startDate < thresh
+//
