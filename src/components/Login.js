53697176e875d0307a03c5b51aff3f351b24031b
Oliwier <oliwier.ostro@gmail.com>
2021-10-29 03:47:47 +0200

    add Login validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,46 +2,50 @@ import { useState } from 'react'
 import styled from 'styled-components'
 import { LoginButton } from 'components/Buttons'
 import { Heading, Caption, Subtext, Entry, Input } from 'components/Forms'
+import { useForm } from 'react-hook-form'
+
+
+const FormContainer = styled.form`
+  margin: auto;`
 
 export default function Login() {
-  const [data, setData] = useState({
-    email: '',
-    password: '',
-  })
+
+  const { register, handleSubmit, formState: { errors }} = useForm()
+
+  const onSubmit = (data) => {
+    console.log(JSON.stringify(data))
+  }
+
   return (
-    <>
+    <FormContainer onSubmit={handleSubmit(onSubmit)}>
       <Heading>log in</Heading>
       <Subtext>Login to your Premiere account below!</Subtext>
-      <Entry>
+      <Entry htmlFor={'email'}>
         <Caption>email address</Caption>
         <Input
-          value={data.email}
-          onChange={(event) =>
-            setData({
-              ...data,
-              email: event.target.value,
-            })
-          }
+          required={ true }
+          {...register('email')} 
+          type={'email'}
           placeholder={'Enter your email address'}
         />
       </Entry>
       <Entry>
         <Caption>password</Caption>
         <Input
+          required={ true }
+          {...register('password', { minLength: 8 })} 
           type={'password'}
-          value={data.password}
-          onChange={(event) =>
-            setData({
-              ...data,
-              password: event.target.value,
-            })
-          }
           placeholder={'Enter your password'}
         />
+        { errors.password && 
+          <p>
+            The password should be at least 8 characters long
+          </p>      
+        }
       </Entry>
-      <Entry style={{ marginTop: 40 }}>
-        <LoginButton />
+      <Entry style={{ marginTop: 40 }}> 
+        <LoginButton type={'submit'}/>
       </Entry>
-    </>
+    </FormContainer>
   )
 }
