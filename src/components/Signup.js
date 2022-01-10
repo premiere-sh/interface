adb812a3db14d6970acc8cc58391c46817be7cf5
oliwier <oliwier.ostro@gmail.com>
2022-01-10 22:35:44 +0100

    add singup branch using formik

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,6 +1,6 @@
-import { useState, useContext } from 'react'
+import { useState } from 'react'
 import { SignupButtonLarge } from 'components/Buttons'
-import { Row, GradientText } from 'components/common'
+import { Row, GradientText, Column } from 'components/common'
 import {
   Heading,
   Caption,
@@ -9,15 +9,12 @@ import {
   Input,
   SmallInput
 } from 'components/Forms'
-import { SignupButtonLarge as SignupButton } from 'components/Buttons'
 import styled from 'styled-components'
-import { useForm } from 'react-hook-form'
 import Link from 'next/link'
-import { useSignUp, useSignIn } from 'hooks'
-import AuthenticationContext from 'contexts/authentication'
 import Router from 'next/router'
 import { Dots } from 'react-activity'
-import WaitingContext from 'contexts/waiting'
+import * as Yup from 'yup'
+import { Formik, Form, Field, useFormik } from 'formik'
 
 const FormContainer = styled.form`
   margin: auto;
@@ -27,15 +24,13 @@ const DateInput = styled(SmallInput)`
   padding-right: 15px;
 `
 
-const RowEntry = styled.div`
+const RowEntry = styled(Row)`
   height: 130px;
   width: 589px;
   margin: auto;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
 `
+
+const ColumnEntry = styled(Column)``
 
 const Alert = styled.div`
   line-height: 17px;
@@ -65,162 +60,147 @@ const ErrorMessageContainer = styled.div`
 `
 
 export default function Signup() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors }
-  } = useForm()
-  const { isAuthenticated, setToken, currentUser } = useContext(
-    AuthenticationContext
+  const passwordValidation = new RegExp(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
   )
-  const [password, setPassword] = useState('')
-  const [errorMessage, setErrorMessage] = useState('')
-  const [waiting, setWaiting] = useContext(WaitingContext)
-  const signUp = useSignUp()
-  const signIn = useSignIn()
 
-  const onSubmit = async (data) => {
-    setErrorMessage('')
-    setWaiting(true)
-    data.date_of_birth = parseInt(new Date(data.date).getTime() / 1000)
-    delete data['date']
-    const { error, token } = await signUp(data)
-    if (error) {
-      setErrorMessage(error)
-      setWaiting(false)
-    } else if (token) {
-      setToken(token)
-      setErrorMessage('')
-      setWaiting(false)
-      Router.push(`/profile`)
+  const passwordMessage =
+    'Password should contain at least one ' +
+    'capital letter, one special character and one number'
+
+  const matchValidation = (password) => {
+    return password == formik.values.password
+  }
+
+  const ageValidation = (dateOfBirth) => {
+    const date18YrsAgo = new Date()
+    date18YrsAgo.setFullYear(date18YrsAgo.getFullYear() - 18)
+    return dateOfBirth <= date18YrsAgo
+  }
+
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      birthDate: '',
+      email: '',
+      password: ''
+    },
+
+    validationSchema: Yup.object({
+      username: Yup.string().required('Username is required'),
+      birthDate: Yup.date()
+        .required('Birthdate is required')
+        .test('isAdult', 'You have to be over 18 years old', (value) =>
+          ageValidation(value)
+        ),
+      email: Yup.string()
+        .email('Email is not valid')
+        .required('Email is required'),
+      password: Yup.string()
+        .required('No password provided')
+        .min(8, 'Password is too short - should be minimum of 8 characters')
+        .matches(passwordValidation, passwordMessage),
+      confirmPassword: Yup.string()
+        .required()
+        .test('confirmPassword', 'Passwords do not match', (value) =>
+          matchValidation(value)
+        )
+    }),
+
+    onSubmit: (values) => {
+      console.log(values)
+      console.log(formik.errors)
     }
+  })
+
+  const onSubmit = (data) => {
+    console.log(data)
   }
 
-  const inUse = (inputValue) => {
-    return true
-  }
-
-  const uppercase = (inputValue) => {
-    const hasUppercase = /(?=.*[A-Z])/
-    return hasUppercase.test(inputValue)
-  }
-
-  const number = (inputValue) => {
-    const hasNumber = /(?=.*[0-9])/
-    return hasNumber.test(inputValue)
-  }
-
-  const special = (inputValue) => {
-    const hasSpecial = /(?=.*[!@#$%^&*])/
-    return hasSpecial.test(inputValue)
-  }
-
-  const adult = (inputValue) => {
-    var today = new Date()
-    var birthDate = new Date(inputValue)
-    var age = today.getFullYear() - birthDate.getFullYear()
-    var m = today.getMonth() - birthDate.getMonth()
-    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-      age--
-    }
-    return age >= 18 ? true : false
-  }
-
-  const checkMatch = (inputValue) => {
-    setPassword(inputValue)
-  }
-
-  const match = (inputValue) => {
-    return inputValue === password ? true : false
-  }
+  console.log(formik.values)
+  console.log(formik.errors)
 
   return (
     <>
-      {!isAuthenticated ? (
-        <FormContainer onSubmit={handleSubmit(onSubmit)}>
-          <Heading>sign up</Heading>
-          <Subtext>Premiere is only available to users that are 18+</Subtext>
-          <RowEntry>
-            <Entry>
-              <Caption>username</Caption>
-              <SmallInput
-                required={true}
-                {...register('username', { validate: { inUse } })}
-                type={'text'}
-                placeholder={'Enter your username'}
-              />
-              {errors.username && <Alert>Username already in use</Alert>}
-            </Entry>
-            <Entry style={{ marginLeft: 65 }}>
-              <Caption>date of birth</Caption>
-              <DateInput
-                required={true}
-                {...register('date', { validate: { adult } })}
-                type={'date'}
-                placeholder={'Enter your date of birth'}
-              />
-              {errors.date && (
-                <Alert>We&apos;re sorry - you must be 18 or older</Alert>
-              )}
-            </Entry>
-          </RowEntry>
+      <form onSubmit={formik.handleSubmit}>
+        <Heading>sign up</Heading>
+        <Subtext>Premiere is only available to users that are 18+</Subtext>
+        <RowEntry>
+          <Entry>
+            <Caption>username</Caption>
+            <SmallInput
+              id="username"
+              type="text"
+              name="username"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              value={formik.values.username}
+            />
+            {formik.touched.username && formik.errors.username ? (
+              <Alert>{formik.errors.username}</Alert>
+            ) : null}
+          </Entry>
+          <Entry style={{ marginLeft: 65 }}>
+            <Caption>date of birth</Caption>
+            <DateInput
+              id="birthDate"
+              type="date"
+              name="birthDate"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              value={formik.values.birthDate}
+            />
+            {formik.touched.birthDate && formik.errors.birthDate ? (
+              <Alert>{formik.errors.birthDate}</Alert>
+            ) : null}
+          </Entry>
+        </RowEntry>
+        <ColumnEntry>
           <Entry>
             <Caption>email address</Caption>
             <Input
-              required={true}
-              {...register('email', { validate: { inUse } })}
-              type={'email'}
-              placeholder={'Enter your email address'}
+              id="emai"
+              type="email"
+              name="email"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              value={formik.values.email}
             />
-            {errors.email && <Alert>Email address already in use</Alert>}
+            {formik.touched.email && formik.errors.email ? (
+              <Alert>{formik.errors.email}</Alert>
+            ) : null}
           </Entry>
           <Entry>
             <Caption>password</Caption>
             <Input
-              required={true}
-              {...register('password', {
-                minLength: 8,
-                validate: { uppercase, number, special, checkMatch, inUse }
-              })}
-              type={'password'}
-              placeholder={'Enter your password'}
+              id="password"
+              type="password"
+              name="password"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              value={formik.values.password}
             />
-            {errors.password?.type === 'minLength' && (
-              <Alert>Please use at least 8 characters</Alert>
-            )}
-            {errors.password?.type === 'uppercase' && (
-              <Alert>Please use at least one capital letter</Alert>
-            )}
-            {errors.password?.type === 'number' && (
-              <Alert>Please use at least one number</Alert>
-            )}
-            {errors.password?.type === 'special' && (
-              <Alert>Please use at least one special character</Alert>
-            )}
-            {errors.password?.type === 'inUse' && (
-              <Alert>Password already in use</Alert>
-            )}
+            {formik.touched.password && formik.errors.password ? (
+              <Alert>{formik.errors.password}</Alert>
+            ) : null}
           </Entry>
           <Entry>
             <Caption>confirm password</Caption>
             <Input
-              required={true}
-              {...register('confirm', { validate: { match } })}
-              type={'password'}
-              placeholder={'Enter your password'}
+              id="confirmPassowrd"
+              type="password"
+              name="confirmPassword"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              value={formik.values.confirmPassword}
             />
-            {errors.confirm && <Alert>Passwords do not match</Alert>}
+            {formik.touched.confirmPassword && formik.errors.confirmPassword ? (
+              <Alert>{formik.errors.confirmPassword}</Alert>
+            ) : null}
           </Entry>
           <SubmitEntry>
-            <SignupButton
-              type={'submit'}
-              disabled={waiting}
-              text={waiting ? <Dots /> : 'sign up'}
-            />
+            <button type="submit"></button>
           </SubmitEntry>
-          {errorMessage && (
-            <ErrorMessageContainer>{errorMessage}</ErrorMessageContainer>
-          )}
           <LoginIfGotAnAccount>
             Already have an account?
             <GradientText style={{ display: 'inline', marginLeft: 5 }}>
@@ -229,10 +209,8 @@ export default function Signup() {
               </Link>
             </GradientText>
           </LoginIfGotAnAccount>
-        </FormContainer>
-      ) : (
-        <div>Authorized!</div>
-      )}
+        </ColumnEntry>
+      </form>
     </>
   )
 }
