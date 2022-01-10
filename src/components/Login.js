d40f7ecf78c80ec82ae8f15bcd78fde16df6b0d2
oliwier <oliwier.ostro@gmail.com>
2022-01-11 00:23:08 +0100

    add formik to login

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,6 +3,8 @@ import { LoginButton } from "components/Buttons"
 import { Heading, Caption, Subtext, Entry, Input } from "components/Forms"
 import { GradientText } from "components/common"
 import Link from "next/link"
+import * as Yup from "yup"
+import { useFormik } from "formik"
 
 const FormContainer = styled.form`
   margin: auto;
@@ -19,17 +21,41 @@ const SignupIfNotGotAnAccount = styled.div`
 `
 
 export default function Login() {
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: ""
+    },
+
+    onSubmit: values => {
+      console.log(values)
+      console.log(formik.errors)
+    }
+  })
+
   return (
-    <FormContainer>
+    <FormContainer onSubmit={formik.handleSubmit}>
       <Heading>log in</Heading>
       <Subtext>Login to your Premiere account below!</Subtext>
       <Entry htmlFor="email">
         <Caption>username</Caption>
-        <Input required type="username" placeholder="Enter your username" />
+        <Input
+          type="text"
+          name="username"
+          onChange={formik.handleChange}
+          value={formik.values.username}
+          placeholder="Enter your username"
+        />
       </Entry>
       <Entry>
         <Caption>password</Caption>
-        <Input required type="password" placeholder="Enter your password" />
+        <Input
+          type="password"
+          name="password"
+          onChange={formik.handleChange}
+          value={formik.values.password}
+          placeholder="Enter your password"
+        />
       </Entry>
       <SubmitEntry>
         <LoginButton type="submit" text="log in" />
