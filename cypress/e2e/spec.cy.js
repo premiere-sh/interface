73440032ff16968a45143febd4cde52bdced2609
Oliwier Ostrowski <oliwier.ostro@gmail.com>
2022-06-30 20:09:16 +0200

    partially implemenet login test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,19 @@
-describe('Index Visiting Test', () => {
-  it('Visists Premiere', () => {
-    cy.visit('http://localhost:3000/')
+describe('The Login Page', () => {
+  it('sets auth cookie when logging in via form submission', () => {
+    // destructuring assignment of the this.currentUser object
+    const userData = { username: 'oliwier', password: 'Szafagra123!' }
+
+    cy.visit('/login')
+
+    cy.get('input[name=username]').type(userData.username)
+
+    // {enter} causes the form to submit
+    cy.get('input[name=password]').type(`${userData.password}{enter}`)
+
+    // we should be redirected to /dashboard
+    cy.url().should('include', '/profile')
+
+    // UI should reflect this user being logged in
+    cy.get().should('contain', 'oliwier')
   })
 })
