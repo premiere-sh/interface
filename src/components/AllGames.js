3b6c39a30bcf553f19675f89ca78f045c9491623
Oliwier Ostrowski <oliwier.ostro@gmail.com>
2022-07-21 23:01:29 +0200

    add filter to tournaments page and apply styles to filter component

--- src/components/AllGames.js
+++ src/components/AllGames.js
@@ -1,56 +1,17 @@
-import Image from 'next/image'
 import styled from 'styled-components'
 import { Grid, Cell } from 'styled-css-grid'
 import GameTile from 'components/GameTile'
-import { Subheading, Row, Container } from 'components/common'
-import GradientDropdown from 'components/GradientDropdown'
-import { useState, useRef, useEffect } from 'react'
+import { Subheading, Row, Container, Column } from 'components/common'
 import {
   InstantSearch,
-  MenuSelect,
   connectHits,
   connectMenu
 } from 'react-instantsearch-dom'
 import algoliasearch from 'algoliasearch'
-import Select from 'react-select'
+import Menu from './Filter'
 
-const DropdownContainer = styled.button`
-  display: flex;
-  align-items: center;
-  flex-direction: row;
-  border: none;
-  outline: none;
-  background: linear-gradient(266.89deg, #982649 -18.13%, #f71735 120.14%);
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-  font-family: Inter;
-  font-style: normal;
-  font-weight: 500;
-  font-size: 16px;
-  line-height: 150%;
-  letter-spacing: 0.1em;
+const GameRow = styled(Row)`
   margin-bottom: 80px;
-  width: 250px;
-`
-
-const DropDownContainer = styled.div`
-  position: absolute;
-`
-
-const HitContainer = styled.div`
-  height: 100px;
-  width: 200px;
-  background: brown;
-`
-
-const Option = styled.option`
-  color: black;
-`
-
-const MenuContainer = styled.div`
-  width: 200px;
-  height: 200px;
-  background: black;
 `
 
 const searchClient = algoliasearch(
@@ -58,33 +19,10 @@ const searchClient = algoliasearch(
   'cd04218c2b5aa38fd04e04f1e56b625a'
 )
 
-const Menu = ({ items, currentRefinement, refine }) => {
-  return (
-    <div>
-      <p>Current refinement: {currentRefinement}</p>
-      <select
-        value={currentRefinement || ''}
-        onChange={(event) => refine(event.currentTarget.value)}
-      >
-        {items.map((item) => (
-          <option
-            key={item.label}
-            value={item.isRefined ? currentRefinement : item.value}
-          >
-            {item.label}
-          </option>
-        ))}
-      </select>
-    </div>
-  )
-}
-
-const CustomMenuSelect = connectMenu(Menu)
-
 const Hits = connectHits(({ hits }) => {
   const items = hits.map((hit, idx) => (
     <Cell style={{ display: 'flex', justifyContent: 'center' }} key={idx}>
-      <GameTile game={hit.name} caption={hit.caption} />
+      <GameTile game={hit.game} caption={hit.caption} />
     </Cell>
   ))
   return (
@@ -94,28 +32,20 @@ const Hits = connectHits(({ hits }) => {
   )
 })
 
+const CustomMenuSelect = connectMenu(Menu)
+
 export default function AllGames({ games }) {
   return (
     <Container>
-      <InstantSearch searchClient={searchClient} indexName="dev_premiere">
-        <Row
+      <InstantSearch searchClient={searchClient} indexName="games">
+        <GameRow
           style={{
             justifyContent: 'space-between'
           }}
         >
           <Subheading>ALL GAMES</Subheading>
-          <DropdownContainer>
-            <div style={{ marginLeft: 22 }}>
-              <Image
-                src={'/dropdown.svg'}
-                width={16}
-                height={16}
-                alt={'dropdown'}
-              />
-            </div>
-            <CustomMenuSelect attribute="platform" />
-          </DropdownContainer>
-        </Row>
+          <CustomMenuSelect attribute="platform" option={'PLATFORM'} />
+        </GameRow>
         <Hits />
       </InstantSearch>
     </Container>
--- src/pages/tournaments.js
+++ src/pages/tournaments.js
@@ -5,49 +5,70 @@ import { Subheading, Row, Column, Container } from 'components/common'
 import Tournament from 'components/Tournament'
 import SmallTournament from 'components/SmallTournament'
 import SocialsSection from 'components/SocialsSection'
-import GradientDropdown from 'components/GradientDropdown'
 import { Grid, Cell } from 'styled-css-grid'
 import { getTournaments } from 'calls'
 import styled from 'styled-components'
+import Menu from 'components/Filter'
+import {
+  InstantSearch,
+  connectHits,
+  connectMenu
+} from 'react-instantsearch-dom'
+import algoliasearch from 'algoliasearch'
 
 const SubheadingRow = styled(Row)`
+  margin-bottom: 80px;
   @media screen and (max-width: 800px) {
     flex-direction: column;
   }
 `
 
-export default function Tournaments({ tournaments }) {
+const searchClient = algoliasearch(
+  'J5N2XCPQQS',
+  '1a824074d4ffa54fc925aaa3ad9a9527'
+)
+const CustomMenuSelect = connectMenu(Menu)
+
+const Hits = connectHits(({ hits }) => {
   const [width, setWidth] = useState(null)
+
   useEffect(function () {
     setWidth(window.innerWidth)
     console.log(window.innerWidth)
   }, [])
+
+  const items = hits.map((hit, idx) => (
+    <Cell style={{ display: 'flex', justifyContent: 'center' }} key={idx}>
+      <SmallTournament tournament={hit} />
+    </Cell>
+  ))
+  return (
+    <div>
+      {width > 800 ? <Tournament tournament={hits[0]} /> : null}
+      <Grid columns={'repeat(auto-fit, minmax(550px, 1fr))'} gap={'50px'}>
+        {items}
+      </Grid>
+    </div>
+  )
+})
+
+export default function Tournaments({ tournaments }) {
   return (
     <Column>
       <Header />
-      <Container>
-        <SubheadingRow style={{ justifyContent: 'space-between' }}>
-          <Subheading>TOURNAMENTS</Subheading>
-          <GradientDropdown text={'FILTER BY GAME'} />
-        </SubheadingRow>
-      </Container>
-      {tournaments && (
-        <>
+      <InstantSearch searchClient={searchClient} indexName="tournaments">
+        <Container>
+          <SubheadingRow style={{ justifyContent: 'space-between' }}>
+            <Subheading>TOURNAMENTS</Subheading>
+            <CustomMenuSelect attribute="game" option={'GAME'} />
+          </SubheadingRow>
+        </Container>
+        {tournaments && (
           <Container style={{ marginBottom: 220 }}>
-            {width > 800 && <Tournament tournament={tournaments[0]} />}
-            <Grid columns={'repeat(auto-fit, minmax(550px, 1fr))'} gap={'50px'}>
-              {tournaments.map((tournament, idx) => (
-                <Cell
-                  style={{ display: 'flex', justifyContent: 'center' }}
-                  key={idx}
-                >
-                  <SmallTournament tournament={tournament} />
-                </Cell>
-              ))}
-            </Grid>
+            <Hits />
           </Container>
-        </>
-      )}
+        )}
+      </InstantSearch>
       <div style={{ marginBottom: 150 }}>
         <SocialsSection />
       </div>
