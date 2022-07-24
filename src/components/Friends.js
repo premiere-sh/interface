5d11d8b60fa22641edfbf547bc9d772e7ad83779
OliwierOstrowski <86793212+OliwierOST@users.noreply.github.com>
2022-07-24 22:21:23 +0200

    Feature: add search friends by algolia on profile page PREM-28 (#40)
    * add algolia filtering to Games page (styling yet to be added)
    
    * add filter to tournaments page and apply styles to filter component
    
    * add sorting users with algolia to leaderboards and unfiy tournaments, games and leaderboard pages layouts
    
    * and algolia key and id to environment variables
    
    * add swich case instead of ternary operators
    
    * add algolia directory and  search client export  in src folder
    
    * add search friends by algolia to profile page

--- src/components/Friends.js
+++ src/components/Friends.js
@@ -2,6 +2,12 @@ import styled from 'styled-components'
 import { Column, Container, Row } from './common'
 import Image from 'next/image'
 import { Grid } from 'styled-css-grid'
+import {
+  InstantSearch,
+  connectHits,
+  connectSearchBox
+} from 'react-instantsearch-dom'
+import { searchClient } from 'algolia/index'
 
 function zip(arrays) {
   return arrays[0].map((_, i) => {
@@ -30,7 +36,7 @@ const Search = styled.input`
   }
 `
 
-const SearchBox = styled(Row)`
+const SearchContainer = styled(Row)`
   justify-content: space-between;
   width: 315px;
   margin-bottom: 90px;
@@ -55,48 +61,76 @@ const Avatar = styled.img`
   border-radius: 170px;
 `
 
+const SearchBox = ({ currentRefinement, refine }) => {
+  return (
+    <Search
+      placeholder={'Search players...'}
+      type="search"
+      value={currentRefinement}
+      onChange={(event) => refine(event.currentTarget.value)}
+    />
+  )
+}
+
+const CustomSearchBox = connectSearchBox(SearchBox)
+
+const Hits = connectHits(({ hits }) => {
+  const items = hits.map((hit, idx) => (
+    <UserColumn style={{ alignItems: 'center' }} key={idx}>
+      <div onClick={() => router.push(`/profile/${friend.id}`)}>
+        <Avatar src={'/devonhenry_.svg'} />
+      </div>
+      <Username>{hit.usernanme}</Username>
+    </UserColumn>
+  ))
+  return (
+    <Grid columns={'repeat(auto-fit, minmax(210px, 1fr))'} gap={'83px'}>
+      {items}
+    </Grid>
+  )
+})
+
 export default function Friends({ friends, invites, avatars }) {
   return (
-    <FriendsContainer>
-      {invites && (
+    <InstantSearch searchClient={searchClient} indexName="users">
+      <FriendsContainer>
+        {invites && (
+          <Container>
+            <TextSection>
+              <YourFriends>Your invites</YourFriends>
+            </TextSection>
+            <Grid columns={'repeat(auto-fit, minmax(210px, 1fr))'} gap={'83px'}>
+              {invites?.length &&
+                avatars?.length &&
+                zip(invites, avatars).map(([invite, avatar], key) => (
+                  <UserColumn style={{ alignItems: 'center' }} key={key}>
+                    <div onClick={() => acceptInvite(invite, token)}>
+                      <Avatar src={avatar} />
+                    </div>
+                    <Username>{invite.inviting_friend}</Username>
+                  </UserColumn>
+                ))}
+            </Grid>
+          </Container>
+        )}
         <Container>
           <TextSection>
-            <YourFriends>Your invites</YourFriends>
+            <YourFriends>Your friends</YourFriends>
+            <SearchContainer>
+              <Image
+                src={'/search.svg'}
+                width={32}
+                height={32}
+                alt={'search'}
+              />
+              <CustomSearchBox />
+            </SearchContainer>
           </TextSection>
           <Grid columns={'repeat(auto-fit, minmax(210px, 1fr))'} gap={'83px'}>
-            {invites?.length &&
-              avatars?.length &&
-              zip(invites, avatars).map(([invite, avatar], key) => (
-                <UserColumn style={{ alignItems: 'center' }} key={key}>
-                  <div onClick={() => acceptInvite(invite, token)}>
-                    <Avatar src={avatar} />
-                  </div>
-                  <Username>{invite.inviting_friend}</Username>
-                </UserColumn>
-              ))}
+            <Hits />
           </Grid>
         </Container>
-      )}
-      <Container>
-        <TextSection>
-          <YourFriends>Your friends</YourFriends>
-          <SearchBox>
-            <Image src={'/search.svg'} width={32} height={32} alt={'search'} />
-            <Search placeholder={'Search players...'} />
-          </SearchBox>
-        </TextSection>
-        <Grid columns={'repeat(auto-fit, minmax(210px, 1fr))'} gap={'83px'}>
-          {friends?.length &&
-            friends.map((friend, key) => (
-              <UserColumn style={{ alignItems: 'center' }} key={key}>
-                <div onClick={() => router.push(`/profile/${friend.id}`)}>
-                  <Avatar src={friend.avatar} />
-                </div>
-                <Username>{friend.username}</Username>
-              </UserColumn>
-            ))}
-        </Grid>
-      </Container>
-    </FriendsContainer>
+      </FriendsContainer>
+    </InstantSearch>
   )
 }
