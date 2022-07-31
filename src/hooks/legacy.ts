0465774d4a7ee87fc42d2c31eb26f3eb23f18ae5
Oliwier Ostrowski <oliwier.ostro@gmail.com>
2022-07-31 19:57:00 +0200

    make legacy.ts from hooks a module by adding an import

--- src/hooks/legacy.ts
+++ src/hooks/legacy.ts
@@ -1,4 +1,4 @@
-// import { useState, useEffect, useContext } from 'react'
+import { useState, useEffect, useContext } from 'react'
 // import AuthenticationContext from 'contexts/authentication'
 
 // export const BASE_URL = 'https://api.premiere.sh/'
