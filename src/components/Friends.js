ef285c15d5e52671e91685acbb4113512a282333
Oliwier Ostrowski <oliwier.ostro@gmail.com>
2022-08-11 16:15:34 +0200

    add buttons for inviting friends on profile page

--- src/components/Friends.js
+++ src/components/Friends.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import { Column, Container, Row } from './common'
+import { Column, Container, Row, Center } from './common'
 import { Button } from 'components/Buttons'
 import Image from 'next/image'
 import { Grid } from 'styled-css-grid'
@@ -10,6 +10,7 @@ import {
 } from 'react-instantsearch-dom'
 import { searchClient } from 'algolia/index'
 import { useState } from 'react'
+import { useRouter } from 'next/router'
 
 function zip(arrays) {
   return arrays[0].map((_, i) => {
@@ -90,6 +91,39 @@ const PopoutContainer = styled(Container)`
   width: 100%;
 `
 
+const AddFriend = styled.div`
+  width: 65px;
+  height: 65px;
+  background: ${(props) => props.theme.colors.white};
+  border-radius: 50%;
+  border: 3px solid #982649;
+  position: absolute;
+  top: 0;
+  right: 15px;
+  z-index: 50;
+`
+
+const Wrapper = styled.div`
+  position: relative;
+`
+
+const ImageContainer = styled(Center)`
+  height: 100%;
+  width: 100%;
+  position: relative;
+`
+
+const InvitationSend = styled.div`
+  display: block;
+  top: 0.05em;
+  left: 0.28em;
+  width: 15%;
+  height: 60%;
+  border: solid #982649;
+  border-width: 0 0.2em 0.2em 0;
+  transform: rotate(45deg);
+`
+
 const SearchBox = ({ currentRefinement, refine }) => {
   return (
     <Search
@@ -103,14 +137,37 @@ const SearchBox = ({ currentRefinement, refine }) => {
 
 const CustomSearchBox = connectSearchBox(SearchBox)
 
-const Hits = connectHits(({ hits }) => {
+const Hits = connectHits(({ hits, refine, currentRefinement }) => {
+  const [invitedFriends, setInvitedFriends] = useState([])
+  const router = useRouter()
+
+  const handleAddFriends = (friend) => {
+    !invitedFriends.includes(friend)
+      ? setInvitedFriends((current) => [...current, friend])
+      : setInvitedFriends((current) =>
+          current.filter((item) => item !== friend)
+        )
+    console.log(invitedFriends)
+  }
+
   const items = hits.map((hit, idx) => (
-    <UserColumn style={{ alignItems: 'center' }} key={idx}>
-      <div onClick={() => router.push(`/profile/${friend.id}`)}>
-        <Avatar src={'/devonhenry_.svg'} />
-      </div>
-      <Username>{hit.usernanme}</Username>
-    </UserColumn>
+    <Wrapper key={idx}>
+      <UserColumn style={{ alignItems: 'center' }}>
+        <div onClick={() => router.push(`/profile/${hit.username}`)}>
+          <Avatar src={'/devonhenry_.svg'} />
+        </div>
+        <Username>{hit.usernanme}</Username>
+      </UserColumn>
+      <AddFriend>
+        <ImageContainer onClick={() => handleAddFriends(hit.usernanme)}>
+          {invitedFriends.includes(hit.usernanme) ? (
+            <InvitationSend />
+          ) : (
+            <Image src={'/cross.svg'} width={32} height={32} alt={'cross'} />
+          )}
+        </ImageContainer>
+      </AddFriend>
+    </Wrapper>
   ))
   return (
     <Grid columns={'repeat(auto-fit, minmax(210px, 1fr))'} gap={'83px'}>
