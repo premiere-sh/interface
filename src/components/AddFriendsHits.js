08b59ae9f4186599a3706dd419df448f28235f4b
Oliwier Ostrowski <oliwier.ostro@gmail.com>
2022-08-25 14:29:14 +0200

    add buttons for accepting and refusing friend requests

--- src/components/AddFriendsHits.js
+++ src/components/AddFriendsHits.js
@@ -1,12 +1,15 @@
 import styled from 'styled-components'
-import { Column, Container, Row, Center } from './common'
-import { Button } from 'components/Buttons'
+import { Column, Center } from './common'
 import Image from 'next/image'
 import { Grid } from 'styled-css-grid'
 import { connectHits } from 'react-instantsearch-dom'
 import { connectStateResults } from 'react-instantsearch-dom'
 import { useState } from 'react'
 import { useRouter } from 'next/router'
+import { sendFriendRequest, cancelFriendRequest } from '../firebase/add-friend'
+import { useUser, useFirestore, useFirestoreCollectionData } from 'reactfire'
+import { collection } from 'firebase/firestore'
+import _ from 'underscore'
 
 const UserColumn = styled(Column)``
 
@@ -38,6 +41,7 @@ const AddFriend = styled.div`
 const Wrapper = styled.div`
   position: relative;
   width: 210px;
+  margin-left: 30px;
 `
 
 const ImageContainer = styled(Center)`
@@ -60,35 +64,56 @@ const InvitationSend = styled.div`
 const Hits = connectHits(({ hits }) => {
   const [invitedFriends, setInvitedFriends] = useState([])
   const router = useRouter()
+  const { data: user } = useUser()
+  const firestore = useFirestore()
+  const friendRequestsCollection = collection(
+    firestore,
+    `users/${user.uid}/friendRequests`
+  )
+  const friendsCollection = collection(firestore, `users/${user.uid}/friends`)
+  const { data: friendRequests } = useFirestoreCollectionData(
+    friendRequestsCollection
+  )
+  const { data: friends } = useFirestoreCollectionData(friendsCollection)
+  const friendRequestsPluck = _.pluck(friendRequests, 'uid')
+  const friendsPluck = _.pluck(friends, 'uid')
+
+  console.log('pluck', friendsPluck)
 
   const handleAddFriends = (friend) => {
-    !invitedFriends.includes(friend)
-      ? setInvitedFriends((current) => [...current, friend])
-      : setInvitedFriends((current) =>
-          current.filter((item) => item !== friend)
-        )
+    if (!invitedFriends.includes(friend)) {
+      setInvitedFriends((current) => [...current, friend])
+      sendFriendRequest(friend)
+    } else {
+      setInvitedFriends((current) => current.filter((item) => item !== friend))
+      cancelFriendRequest(friend)
+    }
     console.log(invitedFriends)
   }
 
-  const items = hits.map((hit, idx) => (
-    <Wrapper key={idx}>
-      <UserColumn style={{ alignItems: 'center' }}>
-        <div onClick={() => router.push(`/profile/${hit.username}`)}>
-          <Avatar src={'/devonhenry_.svg'} />
-        </div>
-        <Username>{hit.usernanme}</Username>
-      </UserColumn>
-      <AddFriend>
-        <ImageContainer onClick={() => handleAddFriends(hit.usernanme)}>
-          {invitedFriends.includes(hit.usernanme) ? (
-            <InvitationSend />
-          ) : (
-            <Image src={'/cross.svg'} width={32} height={32} alt={'cross'} />
-          )}
-        </ImageContainer>
-      </AddFriend>
-    </Wrapper>
-  ))
+  const items = hits.map((hit, idx) =>
+    hit.uid != user.uid &&
+    !friendRequestsPluck.includes(hit.uid) &&
+    !friendsPluck.includes(hit.uid) ? (
+      <Wrapper key={idx}>
+        <UserColumn style={{ alignItems: 'center' }}>
+          <div onClick={() => router.push(`/profile/${hit.username}`)}>
+            <Avatar src={'/devonhenry_.svg'} />
+          </div>
+          <Username>{hit.email}</Username>
+        </UserColumn>
+        <AddFriend>
+          <ImageContainer onClick={() => handleAddFriends(hit.uid)}>
+            {invitedFriends.includes(hit.uid) ? (
+              <InvitationSend />
+            ) : (
+              <Image src={'/cross.svg'} width={32} height={32} alt={'cross'} />
+            )}
+          </ImageContainer>
+        </AddFriend>
+      </Wrapper>
+    ) : null
+  )
   return (
     <Grid columns={'repeat(auto-fit, minmax(210px, 1fr))'} gap={'83px'}>
       {items}
@@ -96,9 +121,7 @@ const Hits = connectHits(({ hits }) => {
   )
 })
 
-const Results = connectStateResults(({ searchState }) =>
-  searchState && searchState.query ? <Hits /> : null
-)
+const Results = connectStateResults(({ searchState }) => <Hits />)
 
 export default function AddFriendsHits() {
   return <Results></Results>
--- src/components/YourFriendsHits.js
+++ src/components/YourFriendsHits.js
@@ -1,9 +1,11 @@
 import styled from 'styled-components'
-import { Column, Container, Row, Center } from './common'
-import { Button } from 'components/Buttons'
+import { Column } from './common'
 import { Grid } from 'styled-css-grid'
 import { connectHits } from 'react-instantsearch-dom'
 import { useRouter } from 'next/router'
+import { useUser, useFirestore, useFirestoreCollectionData } from 'reactfire'
+import { collection } from 'firebase/firestore'
+import _ from 'underscore'
 
 const UserColumn = styled(Column)``
 
@@ -22,21 +24,30 @@ const Avatar = styled.img`
 
 const Wrapper = styled.div`
   position: relative;
+  width: 210px;
+  margin-left: 30px;
 `
 
 export const Hits = connectHits(({ hits }) => {
   const router = useRouter()
+  const { data: user } = useUser()
+  const firestore = useFirestore()
+  const friendsCollection = collection(firestore, `users/${user.uid}/friends`)
+  const { data: friends } = useFirestoreCollectionData(friendsCollection)
+  const friendsPluck = _.pluck(friends, 'uid')
 
-  const items = hits.map((hit, idx) => (
-    <Wrapper key={idx}>
-      <UserColumn style={{ alignItems: 'center' }}>
-        <div onClick={() => router.push(`/profile/${hit.username}`)}>
-          <Avatar src={'/devonhenry_.svg'} />
-        </div>
-        <Username>{hit.usernanme}</Username>
-      </UserColumn>
-    </Wrapper>
-  ))
+  const items = hits.map((hit, idx) =>
+    hit.uid != user.uid && friendsPluck.includes(hit.uid) ? (
+      <Wrapper key={idx}>
+        <UserColumn style={{ alignItems: 'center' }}>
+          <div onClick={() => router.push(`/profile/${hit.username}`)}>
+            <Avatar src={'/devonhenry_.svg'} />
+          </div>
+          <Username>{hit.email}</Username>
+        </UserColumn>
+      </Wrapper>
+    ) : null
+  )
   return (
     <Grid columns={'repeat(auto-fit, minmax(210px, 1fr))'} gap={'83px'}>
       {items}
