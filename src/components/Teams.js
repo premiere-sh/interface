9528aafdb911bbf06c2f5d671da644a8d570ee30
Oliwier Ostrowski <oliwier.ostro@gmail.com>
2022-08-31 17:51:05 +0200

    display teams and team invites on teams tab and add ability to manage invites

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,11 +1,37 @@
 import { useState, useContext } from 'react'
 import styled from 'styled-components'
-import { Column, Container, Row, GradientText, Circle } from 'components/common'
+import {
+  Column,
+  Container,
+  Row,
+  GradientText,
+  Circle,
+  Center
+} from 'components/common'
 import Image from 'next/image'
 import { ArrowButton } from 'components/Buttons'
 import DeleteTeamModal from 'components/DeleteTeamModal'
 import Link from 'next/link'
 import TeamContext from '../contexts/teamsContext'
+import { Button as CreateButton } from 'components/Buttons'
+import {
+  useUser,
+  useFirestore,
+  useFirestoreCollectionData,
+  useFirestoreDocData
+} from 'reactfire'
+import {
+  collection,
+  documentId,
+  query,
+  where,
+  doc,
+  getDocs
+} from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
+import { acceptTeamInvite, refuseTeamInvite } from '../firebase/teams'
+import { Grid } from 'styled-css-grid'
+import _ from 'underscore'
 
 const Button = styled.div``
 
@@ -26,6 +52,7 @@ const Box = styled(Container)`
   margin-top: 86px;
   background: ${(props) => props.theme.colors.white};
   margin-bottom: 50px;
+  width: 100%;
 `
 
 const TeamName = styled.div`
@@ -108,7 +135,7 @@ const ArrowButtonContainer = styled.div`
 
 const IconsRow = styled(Row)``
 
-const TeamsContainer = styled.div``
+const TeamsContainer = styled(Container)``
 
 const EditTeam = styled(GradientText)`
   margin-left: 74px;
@@ -121,69 +148,172 @@ const DeleteTeam = styled(GradientText)`
   margin-right: 17px;
   text-transform: uppercase;
 `
-const teammates = [
-  {
-    user: 'devonhenry_'
-  },
-  {
-    user: 'devonhenry_'
-  },
-  {
-    user: 'devonhenry_'
-  }
-]
 
-const dummyTeams = [
-  {
-    id: 1,
-    name: '[INSERT TEAM NAME]',
-    wins: 123,
-    losses: 4,
-    prem: 1234567
-  },
-  {
-    id: 2,
-    name: '[INSERT TEAM NAME]',
-    wins: 123,
-    losses: 4,
-    prem: 1234567
-  },
-  {
-    id: 3,
-    name: '[INSERT TEAM NAME]',
-    wins: 123,
-    losses: 4,
-    prem: 1234567
-  }
-]
+const CreateTeamButton = styled(CreateButton)`
+  height: 80px;
+  width: 200px;
+`
+
+const InvitesGrid = styled(Grid)`
+  margin-bottom: 30px;
+  margin-left: 30px;
+`
+
+const Wrapper = styled.div`
+  position: relative;
+  width: 210px;
+`
+
+const TeamInvite = styled(Center)`
+  width: 65px;
+  height: 65px;
+  background: ${(props) => props.theme.colors.white};
+  border-radius: 50%;
+  border: 3px solid #982649;
+  position: absolute;
+  top: 0;
+  z-index: 50;
+`
+
+const AcceptTeamInvite = styled(TeamInvite)`
+  right: 15px;
+`
+
+const RefuseTeamInvite = styled(TeamInvite)`
+  left: 15px;
+  transform: rotate(45deg);
+`
+
+const AcceptTeamSign = styled.div`
+  display: block;
+  top: 0.05em;
+  left: 0.28em;
+  width: 15%;
+  height: 60%;
+  border: solid #982649;
+  border-width: 0 0.2em 0.2em 0;
+  transform: rotate(45deg);
+`
+
+const TextSection = styled(Column)``
+
+const YourInvites = styled.div`
+  font-size: 26px;
+  line-height: 31px;
+  font-weight: 600;
+  margin-bottom: 45px;
+`
+
+const NameTeam = styled.div`
+  font-size: 20px;
+  line-height: 24px;
+  font-weight: 500;
+  margin-top: 26.73px;
+`
+
+const Avatar = styled.img`
+  width: 170px;
+  height: 170px;
+  border-radius: 170px;
+`
+
+const UserColumn = styled(Column)``
 
 export default function Teams() {
   const [isOpen, setOpen] = useState(false)
   const [deleteId, setDeleteId] = useState(0)
-  const teamCtx = useContext(TeamContext)
+  const firestore = useFirestore()
+  const { status, data: user } = useUser()
+  const userTeamsCollection = collection(firestore, `users/${user.uid}/teams`)
+  const { data: teams } = useFirestoreCollectionData(userTeamsCollection)
+  const invitesCollection = collection(
+    firestore,
+    `users/${user.uid}/teamInvites`
+  )
+  const { data: teamInvites } = useFirestoreCollectionData(invitesCollection)
 
-  function deleteTeamHandler() {
-    teamCtx.deleteTeam(deleteId)
+  teams?.map(async (team, idx) => {
+    const membersArray = []
+    const membersCollection = collection(
+      firestore,
+      `teams/${team.teamId}/members`
+    )
+    const members = await getDocs(membersCollection)
+    members.docs.map((member) => {
+      membersArray.push(member.data())
+    })
+    teams[idx].members = membersArray
+  })
+
+  const handleAcceptInvite = (id) => {
+    acceptTeamInvite(id)
+  }
+
+  const handleRefuseInvite = (id) => {
+    refuseTeamInvite(id)
   }
 
   return (
     <TeamsContainer>
-      <DeleteTeamModal
-        deleteTeamHandler={deleteTeamHandler}
-        isOpen={isOpen}
-        setOpen={setOpen}
-      />
-
-      {teamCtx.teams?.length &&
-        teamCtx.teams.map((team, key) => (
+      {teamInvites && (
+        <div>
+          <TextSection>
+            <YourInvites>Your invites</YourInvites>
+          </TextSection>
+          <InvitesGrid
+            columns={'repeat(auto-fit, minmax(210px, 1fr))'}
+            gap={'83px'}
+          >
+            {teamInvites?.map((teamInvite, key) => (
+              <Wrapper key={key}>
+                <UserColumn style={{ alignItems: 'center' }}>
+                  <div>
+                    <Avatar src={'/devonhenry_.svg'} />
+                  </div>
+                  <NameTeam>{teamInvite.name}</NameTeam>
+                </UserColumn>
+                <AcceptTeamInvite
+                  onClick={() => handleAcceptInvite(teamInvite.teamId)}
+                >
+                  <AcceptTeamSign />
+                </AcceptTeamInvite>
+                <RefuseTeamInvite
+                  onClick={() => handleRefuseInvite(teamInvite.teamId)}
+                >
+                  <Image
+                    src={'/cross.svg'}
+                    width={32}
+                    height={32}
+                    alt={'cross'}
+                  />
+                </RefuseTeamInvite>
+              </Wrapper>
+            ))}
+          </InvitesGrid>
+        </div>
+      )}
+      <Link href={'/create-team'}>
+        <a>
+          <CreateTeamButton>Create Team</CreateTeamButton>
+        </a>
+      </Link>
+      {teams?.length &&
+        teams.map((team, key) => (
           <Box key={key}>
+            {isOpen == team.teamId && (
+              <DeleteTeamModal
+                isOpen={isOpen}
+                setOpen={setOpen}
+                teamId={team.teamId}
+              />
+            )}
             <Spacer>
               <TeammatesRow>
-                {team.teammates?.length &&
-                  team.teammates.map((teammate, index) => (
+                {team.members?.length &&
+                  team.members.map((member, index) => (
                     <div style={{ marginRight: 27 }} key={index}>
                       <Image
-                        src={`/${teammate.user}.svg`}
+                        src={`/devonhenry_.svg`}
                         width={89.74}
                         height={89.74}
                         alt={'teammate-image'}
@@ -211,7 +341,7 @@ export default function Teams() {
               </IconsRow>
             </Spacer>
             <TeamName>
-              {team.name}: {team.id}
+              {team.name}: {team.teamId}
             </TeamName>
             <TeamInfo>
               <TeamWins>
@@ -227,32 +357,36 @@ export default function Teams() {
                 <Numbers>{team.prem}</Numbers>
               </TeamPrem>
             </TeamInfo>
-            <GradientTextRow>
-              <Link
-                href={{
-                  pathname: 'edit-team',
-                  query: { id: team.id }
-                }}
-              >
-                <a>
-                  <ArrowButtonContainer>
-                    <Button>
-                      <ArrowButton text={'Edit Team'} />
-                    </Button>
-                  </ArrowButtonContainer>
-                </a>
-              </Link>
-              <ArrowButtonContainer>
-                <Button
-                  onClick={() => {
-                    setOpen(true)
-                    setDeleteId(team.id)
+            {team.ownerId == user.uid && (
+              <GradientTextRow>
+                <Link
+                  href={{
+                    pathname: '/edit-team',
+                    query: { teamId: team.teamId }
                   }}
                 >
-                  <ArrowButton text={'Delete Team'} />
-                </Button>
-              </ArrowButtonContainer>
-            </GradientTextRow>
+                  <a>
+                    <ArrowButtonContainer>
+                      <Button>
+                        <ArrowButton text={'Edit Team'} />
+                      </Button>
+                    </ArrowButtonContainer>
+                  </a>
+                </Link>
+                <ArrowButtonContainer>
+                  <Button
+                    onClick={() => {
+                      setOpen(team.teamId)
+                      setDeleteId(team.id)
+                      console.log('setOpen', isOpen)
+                      console.log('teamId', team.teamId)
+                    }}
+                  >
+                    <ArrowButton text={'Delete Team'} />
+                  </Button>
+                </ArrowButtonContainer>
+              </GradientTextRow>
+            )}
           </Box>
         ))}
     </TeamsContainer>
